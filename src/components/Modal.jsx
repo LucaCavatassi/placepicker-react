af16d80c220f715d49cab52bebf67963f0902233
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-09-17 15:42:25 +0200

    feat: modal edit with background working using useEffect dependency

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,22 +1,21 @@
-import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { forwardRef, useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
-const Modal = forwardRef(function Modal({ children }, ref) {
+const Modal = forwardRef(function Modal({ children, open }) {
   const dialog = useRef();
 
-  useImperativeHandle(ref, () => {
-    return {
-      open: () => {
-        dialog.current.showModal();
-      },
-      close: () => {
-        dialog.current.close();
-      },
-    };
-  });
+  useEffect(() => {
+    if (open) {
+      dialog.current.showModal();
+    } else {
+      dialog.current.close();
+    }
+  }, [open])
+
+  // La dipendenza nell'array fa si che l'array parta solo quando open cambia, gli dice parti quando le dipendenze ci sono.
 
   return createPortal(
-    <dialog className="modal" ref={dialog}>
+    <dialog className="modal" ref={dialog} open={open}>
       {children}
     </dialog>,
     document.getElementById('modal')
